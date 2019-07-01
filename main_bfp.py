505f4fb662d483fba0d6df021c0136172895112e
hvdthong <hvdthong@gmail.com>
2019-07-01 18:48:38 +0800

    add files

---
+++ main_bfp.py
@@ -0,0 +1,17 @@
+import pickle
+
+if __name__ == '__main__':
+    # loading data
+    ##########################################################################################################
+    # data = pad_msg, pad_added_code, pad_removed_code, labels, dict_msg, dict_code
+    # pad_msg: padding commit message
+    # pad_added_code: padding added code
+    # pad_removed_code: padding removed code
+    # labels: label of our data, stable or non-stable patches
+    # dict_msg: dictionary of commit message
+    # dict_code: dictionary of commit code
+
+    with open('./data/linux_bfp.pickle', 'rb') as input:
+        data = pickle.load(input)
+    print(len(data))
+    ##########################################################################################################
